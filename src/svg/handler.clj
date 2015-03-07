e6103649ec16f595a999f8a451758097136f3117
john <decker49@gmail.com>
2015-03-07 01:07:11 -0600

    Figured out that I don't need clojure.data.xml; I can use enlive to generate the svg and then it plays well with using enlive for composing and serving up the html.

--- src/svg/handler.clj
+++ src/svg/handler.clj
@@ -3,14 +3,9 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [net.cgrand.enlive-html :as html]))
-(use 'clojure.data.xml)
+;(use 'clojure.data.xml)
 
-(html/deftemplate svg-template "svg/svg.html"
-  [])
-
-(defn make-svg []
-  (emit-str (sexps-as-fragment 
-             [:svg {:width  "100%" 
+(def svg-data [:svg {:width  "100%" 
                     :height "100%"
                     :version "1.1"
                     :xmlns "http://www.w3.org/2000/svg"}
@@ -18,11 +13,24 @@
                       :y "10"
                       :height "100"
                       :width  "100"
-                      :style "stroke:#ff0000; fill: #0000ff"}]])))
+                      :style "stroke:#ff0000; fill: #0000ff"}]])
+
+;; (defn make-svg []
+;;   (emit-str (sexps-as-fragment svg-data)))
+
+(defn make-svg []
+  (html/html svg-data))
+
+(html/deftemplate svg-template "svg/svg.html"
+  []
+  [:title] (html/content "SVG playground")
+  [:body]  (html/content "You should see an svg below.")
+  [:body]  (html/append (make-svg)))
+
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/picture" [] (make-svg))
+  (GET "/picture" [] (svg-template))
   (route/not-found "Not Found"))
 
 (def app
